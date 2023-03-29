2ba8a52285b465c465d0e21988854453c6b2c7f2
AndreaGuarracino <andreaguarracino@outlook.com>
2023-03-29 23:22:37 +0200

    update script, show only corresponding email

--- scripts/author2latex.py
+++ scripts/author2latex.py
@@ -1,5 +1,7 @@
 import csv
 
+drop_contributing_author_emails = True
+
 with open("PGGB Authors - Sheet1.tsv", newline='') as f:
     reader = csv.DictReader(f, delimiter='\t')
 
@@ -20,7 +22,7 @@ with open("PGGB Authors - Sheet1.tsv", newline='') as f:
         if middle:
             first_name += ' ' + middle
         affiliation = r['Affiliations (Department, Organization, Street, City, Postcode, State, Country)'].strip()
-        contribution = r['Contribution (TO FILL BETTER)'].strip()
+        contribution = r['Contribution'].strip()
         rank = float(r["Rank"])
         funding_string = r["Funding"]
         if funding_string != "":
@@ -28,6 +30,9 @@ with open("PGGB Authors - Sheet1.tsv", newline='') as f:
         authors.append((rank, last_name, first_name, affiliation, contribution, email))
 
     authors.sort()
+
+    # First authors have rank < 1
+    co_first = authors[1][0] < 1
     for rank, last_name, first_name, affiliations_string, contributions_string, email in authors:
         # Work out affliations
         affiliations = [i.strip() for i in affiliations_string.split(";")]
@@ -47,7 +52,19 @@ with open("PGGB Authors - Sheet1.tsv", newline='') as f:
                     contribution_dict[contribution] = []
                 contribution_dict[contribution].append(first_name + " " + last_name)
 
-        latex_code += f"\\author[{','.join(affiliation_indices)}]{{\\fnm{{{first_name}}} \\sur{{{last_name}}}}}\\email{{{email}}}\n"
+        # Correspondings have rank < 0
+        corresponding = ''
+        if rank < 0:
+            corresponding = '*'
+
+        email_ = ""
+        if not drop_contributing_author_emails or corresponding:
+            email_ = f"\\email{{{email}}}"
+        latex_code += f"\\author{corresponding}[{','.join(affiliation_indices)}]{{\\fnm{{{first_name}}} \\sur{{{last_name}}}}}{email_}\n"
+
+        # First authors have rank < 1
+        if rank < 1 and co_first:
+            latex_code += "\equalcont{These authors contributed equally to this work.}\n"
 
     for i in range(1, affiliation_index):
         latex_code += f"\\affil[{i}]{{{affiliation_dict_r[i]}}}\n"
