6a399e6346703fd63b95e651f10dab4fe2f82cd8
AndreaGuarracino <andreaguarracino@outlook.com>
2023-04-21 21:03:53 +0200

    sync

--- data/download_SARS-CoV-2.sh
+++ data/download_SARS-CoV-2.sh
@@ -3,6 +3,7 @@
 batch_size=10  # number of lines to process at a time
 line_count=0  # counter for current line number
 batch=()  # array to store current batch of lines
+batch_num=0
 
 while read -r line; do
     if [[ $line_count -eq $batch_size ]]; then
@@ -13,16 +14,20 @@ while read -r line; do
         for element in "${batch[@]}"; do
             # do something with element
             #echo "Processing element: $element"
-	        curl -sS "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${element}&rettype=fasta" >> x.fa
+	        curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${element}&rettype=fasta" >> ${batch_num}.fa
         done
         
         # clear batch
         batch=()
         line_count=0
+        ((batch_num++))
     fi
     # add line to batch
     batch+=("$line")
     ((line_count++))
+
+    # wait for 10 seconds
+    sleep 10
 done < accession_numbers.SARS-CoV-2.txt
 
 # process last batch (if any)
@@ -33,9 +38,9 @@ if [[ $line_count -gt 0 ]]; then
     for element in "${batch[@]}"; do
         # do something with element
         #echo "Processing element
-        curl -sS "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${element}&rettype=fasta" >> x.fa
+        curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${element}&rettype=fasta" >> ${batch_num}.fa
     done
 fi
 
-sed '/^$/d' x.fa > sars_cov_2_sequences.fa
-rm x.fa
+#sed '/^$/d' x.fa > sars_cov_2_sequences.fa
+#rm x.fa
