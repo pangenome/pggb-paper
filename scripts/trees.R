fda13480b20126298d8935a45d2021df67dcdabb
AndreaGuarracino <andreaguarracino@outlook.com>
2023-12-14 14:41:10 -0600

    phylogeny, layouts, cleaning

--- scripts/trees.R
+++ scripts/trees.R
@@ -1,39 +1,52 @@
+#!/usr/bin/env Rscript
+
 library(tidyverse)
 library(dendextend) # for tanglegram
 library(ape) # for phylogenetic analysis
-library(ggtree) # for phylogenetic tree plotting and annotation BiocManager::install("ggtree")
+library(ggtree) # for phylogenetic tree plotting and annotation
 
-path_dist_tsv <- '~/Desktop/pggb-paper/trees/primates16.hsa6_p95.s20000.n16.k79.G700-900-1100.Pasm5.O0001_1.haplotype.dist.tsv'
+# Retrieve command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# Check if we have exactly 3 arguments
+if(length(args) != 3) {
+  stop("Usage: Rscript make_tree.R path_dist_tsv plot_title output_file_path", call. = FALSE)
+}
+
+# Assign arguments
+path_dist_tsv <- args[1]
+plot_title <- args[2]
+output_file_path <- args[3]
 
 # Read matrices
 jaccard_dist_df <- read_tsv(path_dist_tsv) %>%
-  #filter(! group.a %in% c('mSymSyn1#1', 'mSymSyn1#2')) %>%
-  #filter(! group.b %in% c('mSymSyn1#1', 'mSymSyn1#2')) %>%
   arrange(group.a, group.b) %>%
-  #mutate(jaccard=1-jaccard) %>%
   select(group.a, group.b, jaccard.distance) %>%
   pivot_wider(names_from = group.b, values_from = jaccard.distance) %>%
   column_to_rownames(var = "group.a")
-euclidean_dist_df <- read_tsv(path_dist_tsv) %>%
-  arrange(group.a, group.b) %>%
-  select(group.a, group.b, euclidean.distance) %>%
-  pivot_wider(names_from = group.b, values_from = euclidean.distance) %>%
-  column_to_rownames(var = "group.a")
+
+#euclidean_dist_df <- read_tsv(path_dist_tsv) %>%
+#  arrange(group.a, group.b) %>%
+#  select(group.a, group.b, euclidean.distance) %>%
+#  pivot_wider(names_from = group.b, values_from = euclidean.distance) %>%
+#  column_to_rownames(var = "group.a")
 
 jaccard_hc <- as.dist(jaccard_dist_df) %>% hclust()
-euclidean_hc <- as.dist(euclidean_dist_df) %>% hclust()
+#euclidean_hc <- as.dist(euclidean_dist_df) %>% hclust()
 
+# Plot
+pdf(output_file_path) # Start PDF device, replace with svg(output_file_path) for SVG format
 plot(
   jaccard_hc,
-  
   # Label at same height
   hang = -1,
-  main = 'primates16.hsa6',
+  main = plot_title,
   xlab = 'Haplotype',
   ylab = 'Jaccard distance',
   sub = '',
   cex = 1.2
 )
+dev.off() # Close the device
 
 
 if (FALSE) {
